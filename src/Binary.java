5bb53ac022dc472116132d62aeaea47a2513c977
maletskaya <malezkaya@gmail.com>
2015-07-26 23:44:51 +0300

    lesson9

---
+++ src/Binary.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by yanamiletskaya on 24.06.15.
+ */
+public class Binary {
+   public static int i =10;
+
+
+  public static String hexToBinary(){
+      String Hex = new String();
+      int i = Integer.parseInt(Hex);
+      String Bin = Integer.toBinaryString(i);
+      return Bin;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Binary bla = new Binary();
+        Binary.hexToBinary();
+
+    }
+}
