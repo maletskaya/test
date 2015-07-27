d27fd42c1120aa0827bb2c97d3050a9091549e25
maletskaya <malezkaya@gmail.com>
2015-07-27 21:04:53 +0300

    test

---
+++ src/test/java/lesson6/Lesson6.java
@@ -0,0 +1,18 @@
+package lesson6;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Created by yanamiletskaya on 27.07.15.
+ */
+public class Lesson6 {
+    @Test
+    public void seleniumSearchTest(){
+     assertTrue(true);
+        }
+    }
+
+
+//anotation testng testng.org
