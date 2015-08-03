4b3b24fae4c6dcb8d93dffd3d933d00057238e07
maletskaya <malezkaya@gmail.com>
2015-08-03 20:22:41 +0300

    home_work_7

--- src/test/java/ui_tests/lesson7/StylusSearchTest.java
+++ src/test/java/ui_tests/lesson7/StylusSearchTest.java
@@ -1,6 +1,8 @@
 package ui_tests.lesson7;
 
 import core.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import page.StylusSearchPage;
 import static org.testng.Assert.assertTrue;
@@ -14,6 +16,7 @@ public class StylusSearchTest extends TestBase {
     private String stylusWebSite = "http://stylus.com.ua";
     private String filterFrom ="20000";
     private String filterTo ="30000";
+    private WebElement  finaltext;
 
 
     @Test
@@ -29,7 +32,8 @@ public class StylusSearchTest extends TestBase {
         onStylusSearchPage.setFilterFromfield(filterFrom);
         onStylusSearchPage.setFilterTofield(filterTo);
         onStylusSearchPage.clickButtonOK();
-       // assertTrue(?????.getText().contains("Apple iPhone 6 128GB Space Gray")); ?????
+        finaltext = driver.findElement(By.xpath("//*[@id='product-block']/header/div[1]/h1"));
+        assertTrue(finaltext.getText().contains("Смартфон Apple iPhone 6 128GB Space Gray"), "Apple is not found" );
     }
 
 }
